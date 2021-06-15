617839790174d30352180d739fec1470fc3a317e
miskens <miskens@hotmail.com>
2021-06-15 14:09:48 +0200

    minor fixes to responsive pages

--- src/components/Shared/Header.vue
+++ src/components/Shared/Header.vue
@@ -33,7 +33,7 @@ export default {
 .titleDiv {
     width:86%;
     margin-left: 1%;
-    padding: 0vh 3vw;
+    padding: 5% 3vw;
     border-top: 3px solid #1C6EA4;
     border-bottom: 8px double #1C6EA4;
     border-radius: 130px 0px 130px 0px;
@@ -45,11 +45,12 @@ export default {
     margin-bottom:0vh;
 }
 .title {
-    font-weight: 600;
+    font-weight: 500;
     display:inline-block;
     padding:0;
     width:60%;
     font-family: "cassandra";
+    font-size: 1.2rem;
     border-bottom: double;
     border-width: .5rem;
 }
@@ -57,15 +58,46 @@ export default {
     display: inline-block;
     width:60%;
     font-family: "cassandra";
+    font-size: 1rem;
     color: rgb(114, 112, 212);
 }
 img {
     width: 50px;
 }
 }
+@media all and (min-width: 400px) and (min-height:1px) {
+    .titleDiv {
+    padding-top: 2%;
+    padding-bottom: 1%;
+}
+.title {
+    font-size: 1.3rem;
+}
+.name {
+    font-size: 1.1rem;
+}
+}
+@media all and (min-width: 500px) and (min-height:1px) {
+    .titleDiv {
+    padding-top: 1%;
+    padding-bottom: 0%;
+}
+.title {
+    font-size: 1.5rem;
+}
+.name {
+    font-size: 1.2rem;
+}
+}
 @media all and (min-width: 280px) and (max-width: 281px) and (min-height:653px) and (max-height:654px) { /* Galaxy fold */
 .titleDiv {
      width: 91%;
+}
+.title {
+    font-size: 1.4rem;
+}
+.name {
+    font-size: 1rem;
 }
 img {
     width: 50px;
@@ -80,13 +112,19 @@ img {
 .title {
     font-size: 1.2rem;
 }
+.name {
+    font-size: 1rem;
+}
 }
 @media all and (min-width: 320px) and (max-width: 321px) and (min-height:568px) and (max-height:569px) { /*iPhone 5/SE*/
 .titleDiv {
      width: 90%;
 }
 .title {
-    font-size: 1.1rem;
+    font-size: 1.2rem;
+}
+.name {
+    font-size: 1rem;
 }
 img {
     width: 50px;
@@ -102,6 +140,9 @@ img {
 .title {
     font-size: 1.1rem;
 }
+.name {
+    font-size: 1rem;
+}
 }
 @media all and (min-width: 360px) and (max-width: 361px) and (min-height:640px) and (max-height:641px)  { /*Moto 4 & Galaxy S5 */
 .titleDiv {
@@ -109,6 +150,9 @@ img {
 }
 .title {
     font-size: 1.2rem;
+}
+.name {
+    font-size: 1rem;
 }
 img {
     width: 50px;
@@ -123,6 +167,9 @@ img {
 .title {
     font-size: 1.2rem;
 }
+.name {
+    font-size: 1rem;
+}
 }
 @media all and (min-width: 375px) and (max-width: 376px) and (min-height:667px)  and (max-height:668px)  { /*iPhone 6/7/8 */
 .titleDiv {
@@ -130,6 +177,9 @@ img {
 }
 .title {
     font-size: 1.2rem;
+}
+.name {
+    font-size: 1rem;
 }
 img {
     width: 50px;
@@ -144,6 +194,9 @@ img {
 .title {
     font-size: 1.3rem;
 }
+.name {
+    font-size: 1rem;
+}
 }
 @media all and (min-width: 414px) and (max-width: 415px) and (min-height:736px) and (max-height:737px)  { /*iPhone 6/7/8 Plus */
 .titleDiv {
@@ -152,6 +205,9 @@ img {
 }
 .title {
     font-size: 1.2rem;
+}
+.name {
+    font-size: 1rem;
 }
 img {
     width: 50px;
@@ -167,6 +223,9 @@ img {
 .title {
     font-size: 1.2rem;
 }
+.name {
+    font-size: 1rem;
+}
 }
 @media all and (min-width: 375px) and (max-width: 376px) and (min-height:812px) and (max-height:813px)  { /*iPhone X */
 .titleDiv {
@@ -174,6 +233,9 @@ img {
 }
 .title {
     font-size: 1.4rem;
+}
+.name {
+    font-size: 1.2rem;
 }
 img {
     width: 50px;
@@ -187,7 +249,10 @@ img {
     margin-left: 1%;
 }
 .title {
-    font-size: 1.4rem;
+    font-size: 1.2rem;
+}
+.name {
+    font-size: 1rem;
 }
 }
 @media all and (min-width: 411px) and (max-width: 412px) and (min-height:731px) and (max-height:732px) { /*Pixel 2 */
@@ -196,7 +261,10 @@ img {
     margin-left: 0%;
 }
 .title {
-    font-size: 1.4rem;
+    font-size: 1.3rem;
+}
+.name {
+    font-size: 1.1rem;
 }
 img {
     width: 50px;
@@ -212,6 +280,9 @@ img {
 .title {
     font-size: 1.3rem;
 }
+.name {
+    font-size: 1rem;
+}
 }
 @media all and (min-width: 411px) and (max-width: 412px) and (min-height:823px) and (max-height:824px) { /*  Pixel 2 XL */
 .titleDiv {
@@ -220,6 +291,9 @@ img {
 }
 .title {
     font-size: 1.4rem;
+}
+.name {
+    font-size: 1.1rem;
 }
 }
 @media all and (min-width: 823px) and (max-width: 824px) and (min-height:411px) and (max-height:412px) { /* Pixel 2 XL rotated */
@@ -230,6 +304,9 @@ img {
 .title {
     font-size: 1.4rem;
 }
+.name {
+    font-size: 1rem;
+}
 }
 @media all and (min-width: 540px) and (max-width: 541px) and (min-height:720px) and (max-height:721px) { /*Surface Duo*/
 .titleDiv {
@@ -238,6 +315,9 @@ img {
 }
 .title {
     font-size: 1.5rem;
+}
+.name {
+    font-size: 1.2rem;
 }
 }
 @media all and (min-width: 720px) and (max-width: 721px) and (min-height:540px) and (max-height:541px) { /*Surface Duo rotated*/
@@ -248,6 +328,9 @@ img {
 .title {
     font-size: 1.5rem;
 }
+.name {
+    font-size: 1.2rem;
+}
 }
 @media all and (min-width: 768px) and (max-width: 769px) and (min-height: 1024px) and (max-height: 1025px) { /* iPad */
 .titleDiv {
@@ -256,7 +339,7 @@ img {
     padding: 2vh 3vw;
 }
 .title {
-    font-size: 1.8rem;
+    font-size: 2rem;
 }
 }
 @media all and (min-width: 1024px) and (max-width: 1025px) and (min-height: 768px) and (max-height: 769px) { /* iPad rotated*/
@@ -267,6 +350,9 @@ img {
 .title {
     font-size: 1.8rem;
 }
+.name {
+    font-size: 1.3rem;
+}
 }
 @media all and (min-width: 1024px) and (max-width: 1025px) and (min-height: 1366px) and (max-height: 1367px) { /*iPad Pro*/
 .titleDiv {
@@ -274,7 +360,10 @@ img {
     margin-bottom:1vh;
 }
 .title {
-    font-size: 2.4rem;
+    font-size: 2.5rem;
+}
+.name {
+    font-size: 1.8rem;
 }
 img {
     position:relative;
@@ -289,7 +378,10 @@ img {
     margin-bottom:0vh;
 }
 .title {
-    font-size: 2.4rem;
+    font-size: 2.6rem;
+}
+.name {
+    font-size: 2rem;
 }
 img {
     position:relative;
